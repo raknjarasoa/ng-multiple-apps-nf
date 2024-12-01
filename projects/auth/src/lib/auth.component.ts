d6b8ac9ef29a3165ab3ab6762f0cfb104028b5ee
Njarasoa Rakotozafy <raknjarasoa@gmail.com>
2024-12-01 18:23:11 +0100

    add lib auth

---
+++ projects/auth/src/lib/auth.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-auth',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      auth works!
+    </p>
+  `,
+  styles: ``
+})
+export class AuthComponent {
+
+}
